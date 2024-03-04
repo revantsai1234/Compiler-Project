25cdf54b2d28657690f018e367b0d8078533ce7e
revantsai1234 <f20212536@pilani.bits-pilani.ac.in>
2024-03-05 05:15:56 +0530

    lexer.h Final

--- lexer.h
+++ lexer.h
@@ -1,9 +1,15 @@
 #ifndef LEXER_H
 #define LEXER_H
 
-#include "lexerDef.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "lexerDef.h" // Ensure this file contains all necessary type definitions
 
+// Token operations
 const char *getTokenTypeName(TokenType type);
+
+// Hash map operations
 void setNode(struct node *node, char *key, TokenType token);
 void initializeHashMap(struct hashMap *mp);
 int hashFunction(struct hashMap *mp, char *key);
@@ -11,12 +17,15 @@ void insert(struct hashMap *mp, char *key, TokenType token);
 TokenType search(struct hashMap *mp, char *key);
 void insertKeyWords(struct hashMap *mp);
 
-
+// Stream and token processing
 FILE *getStream(FILE *fp, int space_to_fill);
 void checklimits(void);
 TokenInfo getNextToken(void);
 
-void removeComments(char *testcaseFile, char *cleanFile);
-const char *getTokenTypeName(TokenType type); 
+// Comment removal
+void removeComments(char *testcaseFile);
+long getFileSize(FILE* fp);
+void printFileContents(FILE * fp);
+void printLexemes(char *fileName);
 
-#endif
+#endif // LEXER_H
